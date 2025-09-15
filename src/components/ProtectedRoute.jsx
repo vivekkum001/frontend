5e4e17c8182a147238fb19108b17b7614a922067
vivekkum001 <rockymahto9543@gmail.com>
2025-09-15 13:07:22 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 // components/ProtectedRoute.jsx
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+//import React from 'react';
+//import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  return user ? children : <Navigate to="/loginsinup" replace />;
-};
+//const ProtectedRoute = ({ children }) => {
+//  const user = JSON.parse(localStorage.getItem('user'));
+//  return user ? children : <Navigate to="/loginsinup" replace />;
+//};
 
-export default ProtectedRoute;
+//export default ProtectedRoute;
+
